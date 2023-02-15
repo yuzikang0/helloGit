c6c371ddf54e595c66872ed0b1336c21e2e19362
yuzikang0 <49940617+yuzikang0@users.noreply.github.com>
2023-02-15 22:50:27 +0800

    更新1

--- HelloGit/src/main/java/com/atguigu/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/HelloGit.java
@@ -6,5 +6,6 @@ package com.atguigu;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello HelloGit!");
+        System.out.println("更新1");
     }
 }
