a277a8613e116cfcf178ddcb8b39634ecf54d19b
yuzikang0 <49940617+yuzikang0@users.noreply.github.com>
2023-02-16 19:40:01 +0800

    分支添加！

--- HelloGit/src/main/java/com/atguigu/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/HelloGit.java
@@ -8,5 +8,7 @@ public class HelloGit {
         System.out.println("hello HelloGit!");
         System.out.println("更新1");
         System.out.println("创建分支！");
+        System.out.println("分支添加！");
+
     }
 }
