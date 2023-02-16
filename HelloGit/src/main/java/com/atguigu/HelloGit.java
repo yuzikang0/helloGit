8e4c88df11e033d0153e25ce738413e0d2e41267
yuzikang0 <49940617+yuzikang0@users.noreply.github.com>
2023-02-16 20:47:50 +0800

    克隆者添加

--- HelloGit/src/main/java/com/atguigu/HelloGit.java
+++ HelloGit/src/main/java/com/atguigu/HelloGit.java
@@ -12,5 +12,6 @@ public class HelloGit {
         System.out.println("分支添加！");
         System.out.println("在线更新！");
         System.out.println("自己在idea上的添加看能不能上次github");
+        System.out.println("克隆下来的项目添加");
     }
 }
